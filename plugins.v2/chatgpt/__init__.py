198d14157f730451a5a1bc100c7dbc23aff5ccb1
icecauliflower <admin@problemfactory.info>
2026-02-23 17:17:17 -0800

    Update __init__.py

--- plugins.v2/chatgpt/__init__.py
+++ plugins.v2/chatgpt/__init__.py
@@ -17,11 +17,11 @@ class ChatGPT(_PluginBase):
     # 插件图标
     plugin_icon = "Chatgpt_A.png"
     # 插件版本
-    plugin_version = "2.1.8"
+    plugin_version = "2.1.9"
     # 插件作者
-    plugin_author = "jxxghp"
+    plugin_author = "icecauliflower"
     # 作者主页
-    author_url = "https://github.com/jxxghp"
+    author_url = "https://github.com/icecauliflower"
     # 插件配置项ID前缀
     plugin_config_prefix = "chatgpt_"
     # 加载顺序
